7c7ee584741172a731a3d9813af5f5086c14e5d7
Laura S. <lsaro96@gmail.com>
2025-09-27 18:06:15 +0200

    Font changed;

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,33 @@
 $(document).ready(function () {
   lucide.createIcons();
+
+  loadComponent('navbar', 'components/navbar.html').then(() => {
+    initNavbar();
+  });
 });
+
+async function loadComponent(id, url) {
+  const response = await fetch(url);
+  const html = await response.text();
+  document.getElementById(id).innerHTML = html;
+}
+
+function initNavbar() {
+  $(".nav-toggle").click(function () {
+    $("#navbarLinks").toggleClass("active");
+    $(this).toggleClass("open");
+  });
+
+  $("#navbarLinks a").click(function () {
+    $("#navbarLinks").removeClass("active");
+    $(".nav-toggle").removeClass("open");
+  });
+
+  let navToggle = document.querySelector('.nav-toggle');
+  let bars = document.querySelectorAll('.bar');
+
+  navToggle.addEventListener('click', () => {
+    bars.forEach(bar => bar.classList.toggle('x'));
+    navToggle.classList.toggle('open');
+  });
+}
